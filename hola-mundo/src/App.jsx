ea8f28a742e526f4695b2b98d80e30de1bf6615d
anstcher <anastasiacher73@gmail.com>
2024-05-28 22:13:16 +0200

    Creando tarjeta de Twitter

---
+++ hola-mundo/src/App.jsx
@@ -0,0 +1,18 @@
+export function App() {
+    return(
+        <article>
+            <header>
+                <img src="https://unavatar.io/anstsy" alt="El avatar" />
+                <div>
+                    <strong>Anastasia Chernykh</strong>
+                    <span>@cher.anstsy</span>
+                </div>
+            </header>
+
+            <aside>
+                <button>Seguir</button>
+            </aside>
+
+        </article>
+    )
+}
